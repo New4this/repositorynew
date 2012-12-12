4fb3aa77b84102c3979cd19222211d066cf930ef
New2this <rk00112@surrey.ac.uk>
2012-12-12 11:19:38 +0000

    ready to push to Heroku

---
+++ aaa_website/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AAAWebsite::Application.config.secret_token = '8146dde5b410cd6be84def214dfe2c1869f477a60d9486f4a9c43e0d00b89febb20be96ce97bf9714dd296a68020d373b089d1486f3cdb0334b4cc0e98756f1c'
